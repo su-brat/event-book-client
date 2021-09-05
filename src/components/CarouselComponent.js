03cda309e8166c27161b9614b9b175272cc052fb
subrat1729 <subratpatnaik.email@gmail.com>
2021-09-05 17:49:32 +0530

    Carousel height fixed

--- src/components/CarouselComponent.js
+++ src/components/CarouselComponent.js
@@ -9,9 +9,10 @@ export default function CarouselComponent(props) {
                     props.images.map((image, index) => (
                         <Carousel.Item key={image.url}>
                             <img
-                                className="d-block w-100"
+                                className="d-block mx-auto"
                                 src={image.url}
                                 alt="Not found"
+                                height="550px"
                             />
                             <Carousel.Caption>
                                 <h3>Image {index+1}</h3>
@@ -22,9 +23,10 @@ export default function CarouselComponent(props) {
                 ) : (
                     <Carousel.Item>
                         <img
-                            className="d-block w-100"
+                            className="d-block mx-auto"
                             src=""
                             alt="No Images"
+                            height="550px"
                         />
                         <Carousel.Caption>
                             <h3>No Images</h3>
