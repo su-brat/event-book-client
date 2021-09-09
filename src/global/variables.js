3c8a2c1682403e1972fb7a5fa977e02c0e511be2
subrat1729 <subratpatnaik.email@gmail.com>
2021-09-10 02:01:07 +0530

    Deploy mode hardcoded

--- src/global/variables.js
+++ src/global/variables.js
@@ -1,2 +1,2 @@
 // export const API_BASE = 'http://10.0.2.2:3001';
-export const API_BASE = process.env.MODE==='production'?process.env.SERVER_DOMAIN:'http://localhost:3001';
+export const API_BASE = 'https://festan-server.herokuapp.com/';
