1adf868f1e58358b43be6ff258c58bcf1654f29e
subrat1729 <subratpatnaik.email@gmail.com>
2021-09-10 01:49:08 +0530

    production

--- src/global/variables.js
+++ src/global/variables.js
@@ -1,2 +1,2 @@
 // export const API_BASE = 'http://10.0.2.2:3001';
-export const API_BASE = process.env.NODE_ENV==='PRODUCTION'?process.env.SERVER_DOMAIN:'http://localhost:3001';
+export const API_BASE = process.env.NODE_ENV==='production'?process.env.SERVER_DOMAIN:'http://localhost:3001';
